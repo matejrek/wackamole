9351aafca838485c9ae89aaa94e9cc90114474a6
matejrek <matej_rek@ymail.com>
2023-03-03 17:30:30 +0100

    Added dynamic grid size, wacks counter

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,29 @@
 import './App.css'
 import { create } from 'zustand'
 import { useEffect, useState } from 'react'
+import Controls from './components/controls'
 
 export const useMoleStore = create((set) => ({
   running: false,
+  size: 3,
   toggleRun: () => set((state: any) => ({ running: !state.running })),
+  setSize: (size: any) => set({ size }),
 }))
 
 function App() {
   const [position, setPosition] = useState(0)
   const [points, setPoints] = useState(0)
   const [lastMove, setLastMove] = useState('')
+  const [wacks, setWacks] = useState(0)
 
   const isRunning = useMoleStore((state: any) => state.running)
-  const toggleRun = useMoleStore((state: any) => state.toggleRun)
+  const isGridSize = useMoleStore((state: any) => state.size)
 
 
   useEffect(() => {
     const timerId = setInterval(() => {
       if (isRunning) {
-        setPosition(Math.floor(Math.random() * 9 + 1))
+        setPosition(Math.floor(Math.random() * (isGridSize * isGridSize) + 1))
       }
       else {
         setPosition(0)
@@ -36,40 +40,46 @@ function App() {
   }, [lastMove]);
 
   function wackIt(num: number) {
+    setWacks(wacks + 1)
     if (num == position) {
-      console.log("1 point to Gryffindor");
       setPoints(points + 1)
       setLastMove("1 point to Gryffindor")
     }
     else {
-      console.log("Wacked and failed!")
       setLastMove("Wacked and failed!")
     }
   }
 
+  const rows: any = [];
+
+  for (let i = 1; i <= isGridSize * isGridSize; i++) {
+    rows.push(<button key={"wack-" + i} className={position == i ? 'active' : ''} onClick={() => wackIt(i)}></button>);
+  }
+
+  let frs = "";
+  for (let i = 0; i < isGridSize; i++) {
+    frs += "1fr ";
+  }
+  let gridLayoutCss = {
+    gridTemplateColumns: frs,
+    width: isGridSize * 50,
+    height: isGridSize * 50
+  }
 
   return (
     <div className="App">
-      <h1>Wack a mole <br />Points: ({points})</h1>
+      <h1>Wack a mole <br />Points: {points} ({wacks} wacks)</h1>
       <h3>Currently {isRunning ? 'running' : 'not running'}</h3>
-      <button onClick={toggleRun}>Run/Pause</button>
-      <div className="grid">
-        <button className={position == 1 ? 'active' : ''} onClick={() => wackIt(1)}></button>
-        <button className={position == 2 ? 'active' : ''} onClick={() => wackIt(2)}></button>
-        <button className={position == 3 ? 'active' : ''} onClick={() => wackIt(3)}></button>
-        <button className={position == 4 ? 'active' : ''} onClick={() => wackIt(4)}></button>
-        <button className={position == 5 ? 'active' : ''} onClick={() => wackIt(5)}></button>
-        <button className={position == 6 ? 'active' : ''} onClick={() => wackIt(6)}></button>
-        <button className={position == 7 ? 'active' : ''} onClick={() => wackIt(7)}></button>
-        <button className={position == 8 ? 'active' : ''} onClick={() => wackIt(8)}></button>
-        <button className={position == 9 ? 'active' : ''} onClick={() => wackIt(9)}></button>
+      <Controls />
+      <div className="grid" style={gridLayoutCss}>
+        {rows}
       </div>
 
       <h1 className={lastMove == "1 point to Gryffindor" ? 'lastMove green' : 'lastMove red'}>
         {lastMove != "" ? lastMove : ''}
       </h1>
 
-    </div>
+    </div >
   )
 }
 
--- src/components/controls.tsx
+++ src/components/controls.tsx
@@ -1,10 +1,20 @@
-
+import { useMoleStore } from "../App";
 
 function Controls() {
+  const toggleRun = useMoleStore((state: any) => state.toggleRun)
+  const gridSize = useMoleStore((state: any) => state.size)
+  const setSize = useMoleStore((state: any) => state.setSize)
+
 
   return (
     <>
-      <h2>Controls:</h2>
+      <div className="controls">
+        <button onClick={toggleRun}>Run/Pause</button>
+        <div className="control-group">
+          <label>Grid size:</label>
+          <input id="grid-size" type="number" value={gridSize} onChange={(e) => setSize(e.target.value != "" ? e.target.value : 3)} />
+        </div>
+      </div>
 
     </>
   );
